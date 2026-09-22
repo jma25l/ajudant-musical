46108f564af6e572d372d5d16f16f737a1468042
Joaquín Millán Aldaz <jma25l@outlook.es>
2026-09-22 21:09:39 +0200

    Menors

--- app/visor/page.tsx
+++ app/visor/page.tsx
@@ -7,6 +7,8 @@ import { useEffect, useState } from "react";
 import "./estil.scss";
 import { cercaAcordsConeguts } from "@/lib/busca";
 
+const VALID_FILE_TYPES = ["text/plain", "text/markdown"];
+
 export default function Visor() {
   const [lletra, setLletra] = useState<string | undefined>(undefined);
   const [coneguts, setConeguts] = useState<AcordsDBList>({});
@@ -18,21 +20,22 @@ export default function Visor() {
     }
     const fitxer = e.target.files[0];
 
-
-    if (!["text/plain", "text/markdown"].includes(fitxer.type)) {
-      console.log(fitxer.type)
+    if (!VALID_FILE_TYPES.includes(fitxer.type)) {
+      console.log(fitxer.type);
       setLletra(undefined);
       return;
     }
     const lector = new FileReader();
-    lector.onload = (t) => { setLletra(t.target?.result as string) };
-    
+    lector.onload = (t) => {
+      setLletra(t.target?.result as string);
+    };
+
     lector.readAsText(fitxer);
   };
 
-    useEffect(() => {
-      cercaAcordsConeguts().then(setConeguts);
-    }, []);
+  useEffect(() => {
+    cercaAcordsConeguts().then(setConeguts);
+  }, []);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -40,7 +43,12 @@ export default function Visor() {
         Inici
       </Link>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start py-16 px-16 bg-white dark:bg-black sm:items-start">
-        <input type="file" onChange={canviFitxer} /> <br/>
+        <input
+          type="file"
+          onChange={canviFitxer}
+          accept={VALID_FILE_TYPES.join(",")}
+        />{" "}
+        <br />
         {lletra ? (
           <AcordsCanco nom={"Local"} lletra={lletra} coneguts={coneguts} />
         ) : (
